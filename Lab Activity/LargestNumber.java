841890fc4ebb248084cecbecb29700e86e31db72
AniketB2507 <107405361+AniketB2507@users.noreply.github.com>
2022-06-23 22:31:18 +0530

    Add files via upload

---
+++ Lab Activity/LargestNumber.java
@@ -0,0 +1,25 @@
+public class LargestNumber 
+{
+
+	public static void main(String[] args) {
+		int p=70;
+		int q=73;
+		int r=79;
+                int s=425;  
+		
+		if(p>q && p>r) 
+			
+			 System.out.println(p + " is the largest number.");
+			 else if (q>= p && q >= r)
+				 
+		      System.out.println(q + " is the largest   number.");
+                 else if(r>s && r>=p)
+                System.out.println(r + " is the largest number.");
+
+		        else
+		            System.out.println(s + " is the largest number.");
+			
+		
+	}
+
+}
