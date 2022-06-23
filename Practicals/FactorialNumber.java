d5c1dc4376dbe379eadf7eda21cb91906e3be090
AniketB2507 <107405361+AniketB2507@users.noreply.github.com>
2022-06-23 22:46:33 +0530

    Add files via upload

---
+++ Practicals/FactorialNumber.java
@@ -0,0 +1,18 @@
+//write a program to print factorial number :-
+
+public class FactorialNumber
+{  
+
+ public static void main(String args[]){  
+
+  int i,fact=1;
+  
+  int number=5;//It is the number to calculate factorial   
+ 
+  for(i=1;i<=number;i++){    
+
+      fact=fact*i;    
+  }    
+  System.out.println("Factorial of "+number+" is: "+fact);    
+ }  
+}
