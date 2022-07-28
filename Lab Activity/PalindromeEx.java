458bc8b6389c8ee2720f965b620b2f7cc3054fe8
AniketB2507 <107405361+AniketB2507@users.noreply.github.com>
2022-07-28 18:26:56 +0530

    Add files via upload

---
+++ Lab Activity/PalindromeEx.java
@@ -0,0 +1,22 @@
+/*
+ * @ Author Aniket Ingale
+ */
+package Lab28July;
+
+public class PalindromeEx {
+	
+		public static void main(String args[]) {
+			StringBuffer sb= new StringBuffer("i am a good boy");
+			String c=sb.toString();
+			
+			sb.reverse();
+			
+			if(c.contentEquals(sb)) {
+				System.out.println("palindrome");
+			}
+			else {
+				System.out.println("not a palindron");
+			}
+			
+		}
+		}
